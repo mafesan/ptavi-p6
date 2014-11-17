014bcd1b94a452aeae730532aae4e9ea755638e3
igarag <naxo.elderane@gmail.com>
2014-11-17 16:47:14 +0100

    Servidor v2.0

--- server.py
+++ server.py
@@ -6,24 +6,32 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import SocketServer
 import sys
-import os.path
+import os
 
 
-SERVER_IP = sys.argv[1]
-SERVER_PORT = int(sys.argv[2])
-FILE = sys.argv[3]
+if len(sys.argv) == 4:
+    SERVER_IP = sys.argv[1]
+    SERVER_PORT = int(sys.argv[2])
+    FILE = sys.argv[3]
+    if not os.path.isfile(FILE):
+        sys.exit("Usage: python server.py IP port audio_file")
+else:
+    sys.exit("Usage: python server.py IP port audio_file")
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+
 
     def handle(self):
+        """
+        Server SIP
+        """
+        print "Listening..."
+
         client_address = self.client_address[0]
-        client_port = self.client_address[1]
+        client_port = int(self.client_address[1])
 
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+        self.wfile.write("Hemos recibido tu peticion\r\n\r\n")
 
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente.
@@ -31,17 +39,20 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             if not line:
                 break
             else:
-                print "El cliente nos manda " + line
-                # Si no hay más líneas salimos del bucle infinito
-
+                print "Mensaje de entrada " + line
+                line = line.split()
+                line[1] = line[1].split(":")
+                if line[0] == "INVITE" and line[2] == "SIP/2.0":
+                    self.wfile.write("SIP/2.0 100 Trying\r\n\r\n" 
+                                        + "SIP/2.0 180 Ring\r\n\r\n" 
+                                        + "SIP/2.0 200 OK\r\n\r\n")
+                elif line[0] == "ACK":
+                    print "Comienza la transmision........."
+                    Streaming = './mp32rtp -i 127.0.0.1 -p 23032 < ' + FILE
+                    os.system(Streaming)
+                    print "Fin de la emision"
 #===================== PROGRAMA PRINCIPAL ====================================
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        if not os.path.isfile(FILE):
-            sys.exit("No existe el archivo")
-    else: 
-        print "Usage: python server.py IP port audio_file"
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", SERVER_PORT), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
