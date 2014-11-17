f18aa8b0338d432ee6ce34304f86133e9d9029d4
igarag <naxo.elderane@gmail.com>
2014-11-17 10:54:30 +0100

    cliente.py v1.0

--- client.py
+++ client.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 """
-Programa cliente que abre un socket a un servidor
+IGNACIO ARRANZ AGUEDA - ISAM - PTAVI - PRACTICA 6
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+# CAMPOS => METODO | DIRECCION SIP
+if len(sys.argv) == 3:
+	METODO = sys.argv[1]
+	DIRECCION = sys.argv[2]
+else:
+	print "Usage: python client.py method receiver@IP:SIPport"
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+# Proceso de corte de la variable DIRECCION.
+DIRECCION = DIRECCION.split("@")
+NAME = DIRECCION[0]
+DIRECCION = DIRECCION[1].split(":")
+# IP y PUERTO del receptor
+IP_SERVER = DIRECCION[0]
+PORT = int(DIRECCION[1])
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
-
+LINE = METODO + " sip:" + NAME + "@" + IP_SERVER + " SIP/2.0"
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP_SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
-
-print 'Recibido -- ', data
-print "Terminando socket..."
-
-# Cerramos todo
-my_socket.close()
-print "Fin."
+try:
+	print "Enviando: " + LINE
+	my_socket.send(LINE + '\r\n')
+	data = my_socket.recv(1024)
+	print 'Recibido -- ', data
+	# Cerramos todo
+	my_socket.close()
+	print "Fin."
+except:
+	print "Error: No server listening at", IP_SERVER, "port",PORT
