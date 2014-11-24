67e59399ec1dba6bc5efd83d16f0d292c4528a8d
lyanezgu <lyanezgu@gmail.com>
2014-11-24 08:44:46 +0100

    servidor comprobado pep8

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import SocketServer
 import sys
 import os
 
+
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
@@ -15,14 +16,14 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+        #self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print "El cliente nos manda " + line
-            linea = line.split()
+            linea = line.split(" ")
             metodo = linea[0]
-            if len(sys.argv) != 3:
+            if len(linea) != 3:
                 self.wfile.write("SIP/2.0 400 Bad Request" + '\r\n')
             else:
                 if metodo == 'INVITE':
@@ -31,31 +32,31 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                     self.wfile.write("SIP/2.0 200 OK" + '\r\n')
                 elif metodo == 'ACK':
                     print "ACK recibido..."
-                    aEjecutar = 'mp32rtp -i ' + sys.argv[1] + '-p 23032 ' + \
+                    aEjecutar = './mp32rtp -i ' + sys.argv[1] + ' -p 23032 ' +\
                                 '< ' + sys.argv[3]
+                    print aEjecutar
                     print "Envio de RTP..."
                     os.system(aEjecutar)
                 elif metodo == 'BYE':
                     print "BYE recibido..."
-                    self.wfile.write("SIP/2.0 200 OK")     
+                    self.wfile.write("SIP/2.0 200 OK")
                 elif metodo != 'INVITE' or 'ACK' or 'BYE':
                     self.wfile.write("SIP/2.0 405 Method Not Allowed" + '\r\n')
-            
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+                # Si no hay más líneas salimos del bucle infinito
+                if not line or "[""]":
+                    break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     if len(sys.argv) != 4:
         sys.exit("Usage: python server.py IP port audio_file")
-    
+
     IP = sys.argv[1]
-    PUERTO = sys.argv[2]
+    PUERTO = int(sys.argv[2])
     archivo = sys.argv[3]
-    try: 
-        fichero = open(archivo) 
-    except: 
+    try:
+        fichero = open(archivo)
+    except:
         print "Usage: python server.py IP port audio_file"
 
     print "Listening..."
